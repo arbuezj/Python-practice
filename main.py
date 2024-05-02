082c0bfce5bc38e1ea05547591c7f1a961c39f68
arbuezj <159852349+arbuezj@users.noreply.github.com>
2024-05-02 12:28:52 +1200

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+# Ask the user thier name and save it 
+input("what's your name?")
+# Greet the user and the introduce the quiz
+print("welcome to the quiz")
+print("This quiz is about type of car")
+# Ask the user a question 
+input("what is the most expinse car ")
+# Tell them the correct answer 
+print("The most expensive car is rolls-royce droptail it cost 30 million plus")
+#end the quiz
+input("thank you for playing")
