b8a257e421372e3d1123dcf380b090ab91b89b44
arbuezj <159852349+arbuezj@users.noreply.github.com>
2024-05-10 13:00:01 +1200

    v 3

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 score = 0
+QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
 
 # Ask the user thier name and save it 
 name = input("what's your name?")
@@ -7,16 +8,21 @@ print("welcome to the quiz",name)
 
 print("This quiz is about type of car")
 # Ask the user a question 
-
-answer = input("what is the most expinsive car ").lower()
+question= "What is the most expinsive car?"
+a = "lamborgini"
+b = "Honda Civic"
+c = "Pajero Sport"
+d = "Rolls-royce droptail"
+answer = input(QUESTION_FORMAT.format(question , a, b, c ,d)).lower()
 # Tell them the correct answer 
-if answer == "rolls-royce droptail".lower():
+if answer == d.lower() or answer == "d":
     print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are correct!")
     score +=10
 elif answer == "":
     print(" you dont know ")
-  
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c":
 else:
+    print ("Wrong!")
     print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are incorrect")
 
 #end the quiz
