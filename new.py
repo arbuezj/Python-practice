e9a02d0869412198b7a460fd16bdcdd176ba188c
arbuezj <159852349+arbuezj@users.noreply.github.com>
2024-05-14 12:28:54 +1200

    new

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
 score = 0
 QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
+play = "yes" 
 
 # Ask the user thier name and save it 
 name = input("what's your name?")
@@ -7,24 +8,32 @@ name = input("what's your name?")
 print("welcome to the quiz",name)
 
 print("This quiz is about type of car")
-# Ask the user a question 
-question= "What is the most expinsive car?"
-a = "lamborgini"
-b = "Honda Civic"
-c = "Pajero Sport"
-d = "Rolls-royce droptail"
-answer = input(QUESTION_FORMAT.format(question , a, b, c ,d)).lower()
-# Tell them the correct answer 
-if answer == d.lower() or answer == "d":
-    print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are correct!")
-    score +=10
-elif answer == "":
-    print(" you dont know ")
-elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c":
-else:
-    print ("Wrong!")
-    print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are incorrect")
 
-#end the quiz
-print("Well done{} thank you for playing thats end .Your final score was {} ".format (name,score))
+tries = input("how many attemps do you want at each question? 1-4")
+tries = tries - 1
+
+# Ask the user a question 
+while play == "yes" :
+    question= "What is the most expinsive car?"
+    a = "lamborgini"
+    b = "Honda Civic"
+    c = "Pajero Sport"
+    d = "Rolls-royce droptail"
+    answer = input(QUESTION_FORMAT.format(question , a, b, c ,d)).lower()
+    # Tell them the correct answer 
+    if answer == d.lower() or answer == "d":
+        print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are correct!")
+        score +=10
+    elif answer == "":
+        print(" you dont know ")
+    elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c":
+        print (" that's not a option")
+    else:
+        print ("Wrong!")
+        print("The most expensive car is rolls-royce droptail it cost 30 million plus. You are incorrect")
+
+    #end the quiz
+    print("Well done{} thank you for playing thats end .Your final score was {} ".format (name,score))
+
+    play = input("do you want to play agin?").lower()
 
